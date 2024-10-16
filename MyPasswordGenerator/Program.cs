b31d0f38a904db14dcbbf4f2920510a90ced90f0
peiyao lai <plai@wpi.edu>
2024-10-16 16:09:58 -0400

    Initial commit: MyFirstConsoleApp and MyPasswordGenerator.

---
+++ MyPasswordGenerator/Program.cs
@@ -0,0 +1,8 @@
+﻿// By default, all characters available for use and a length of 16
+// Will return a random password with the default settings 
+using PasswordGenerator;
+
+// var pwd = new Password(includeLowercase: true, includeNumeric: true, includeUppercase: true, includeSpecial: false, passwordLength: 16);
+var pwd = new Password(17).IncludeLowercase().IncludeNumeric().IncludeUppercase().IncludeSpecial("[]{}!^_=");
+var password = pwd.Next();
+Console.WriteLine(password.ToString());
